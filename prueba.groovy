10d8fcc808c30cf5383260f0a343aac85fa5967e
juanmazonflo <91501671+juanmazonflo@users.noreply.github.com>
2022-04-19 18:33:29 -0700

    Create prueba.groovy

---
+++ prueba.groovy
@@ -0,0 +1,60 @@
+job('Java Prueba App DSL 3') {
+    description('Java Maven App con DSL para el curso de Jenkins')
+    scm {
+        git('https://github.com/LuisRuizLemas/Pruebas-DSL3.git', 'main') { node ->
+            node / gitConfigName('LuisRuizLemas')
+            node / gitConfigEmail('luisgjenkins@gmail.com')
+        }
+    }
+    triggers {
+    	githubPush()
+    }    
+    steps {
+        maven {
+          mavenInstallation('mavenjenkins')
+          goals('-B -DskipTests clean package')
+        }
+        maven {
+          mavenInstallation('mavenjenkins')
+          goals('test')
+        }
+        shell('''
+          echo "Entrega: Desplegando la aplicación" 
+          java -jar "/var/jenkins_home/workspace/Java Maven App DSL 3/target/my-app-1.0-SNAPSHOT.jar"
+        ''')  
+    }
+    publishers {
+        archiveArtifacts('target/*.jar')
+        archiveJunit('target/surefire-reports/*.xml')
+	      slackNotifier {
+            notifyAborted(true)
+            notifyEveryFailure(true)
+            notifyNotBuilt(false)
+            notifyUnstable(false)
+            notifyBackToNormal(true)
+            notifySuccess(true)
+            notifyRepeatedFailure(false)
+            startNotification(false)
+            includeTestSummary(false)
+            includeCustomMessage(false)
+            customMessage(null)
+            sendAs(null)
+            commitInfoChoice('NONE')
+            teamDomain(null)
+            authToken(null)
+       }
+    }
+}
+
+job('Job test Hola Mundo') {
+	description('Aplicacion Hola Mundo de Prueba')
+	triggers {
+		cron('H/6 * * * *')
+    		githubPush()
+    	}
+	steps {
+		shell('''
+			echo "Hoooola Mundo!!!" 
+		''')
+	}
+}
